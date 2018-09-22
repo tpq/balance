06f2f5bbb3503bcc61d77356637dfd825e9109a7
Thom Quinn <contacttomquinn@gmail.com>
2018-09-22 13:20:58 +1000

    Fix test because sbp.fromHclust now names balances

--- tests/testthat/test-sbp.R
+++ tests/testthat/test-sbp.R
@@ -4,6 +4,7 @@ h <- hclust(dist(cars[1:35,]))
 phylo <- ape::as.phylo(h)
 sbp1 <- philr::phylo2sbp(phylo)
 sbp2 <- sbp.fromHclust(h)
+colnames(sbp2) <- NULL
 
 test_that("sbp.fromHclust matches the ape and philr method", {
 
