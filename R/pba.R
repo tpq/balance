57e9107b6446208e8ffe385d344cf118c0ed3d89
Thom Quinn <contacttomquinn@gmail.com>
2018-09-26 15:46:43 +1000

    Add pba.show and pba.plot methods

--- R/pba.R
+++ R/pba.R
@@ -1,5 +1,11 @@
 #' A pba model S4 class
 #'
+#' @param object,x A \code{pba} object.
+#' @param y A matrix on which to deploy the \code{pba} model.
+#' @param group A character vector. Group labels used to color points.
+#' @param pb1,pb2 An integer. Sets principal balances to plot.
+#' @param size.text An integer. Sets legend text size.
+#'
 #' @slot data A matrix. The original data.
 #' @slot sbp A matrix. The SBP matrix.
 #' @slot pba A matrix. The balances.
@@ -47,17 +53,95 @@ pba <- function(x, alpha = NA){
   object
 }
 
+#' @describeIn pba Method to show \code{pba} model.
+#' @export
+setMethod("show", "pba",
+          function(object){
+
+            cat("###PBA object\n")
+
+            cat("@data summary:",
+                nrow(object@data), "samples by", ncol(object@data), "components\n")
+
+            cat("@sbp summary:",
+                nrow(object@sbp), "components by", ncol(object@sbp), "balances\n")
+
+            cat("@pba summary:",
+                nrow(object@pba), "samples by", ncol(object@pba), "balances\n")
+
+            cat("-z1:",
+                paste0(round(object@subvar[1]*100, 2), "%"), "variance explained\n")
+            if(ncol(object@sbp) > 1){
+              cat("-z2:",
+                  paste0(round(object@subvar[2]*100, 2), "%"), "variance explained\n")
+            }
+            if(ncol(object@sbp) > 2){
+              cat("-z3:",
+                  paste0(round(object@subvar[3]*100, 2), "%"), "variance explained\n")
+            }
+          }
+)
+
 #' @describeIn pba Method to deploy \code{pba} model.
-#'
-#' @param object A \code{pba} object.
-#' @param newdata A matrix.
-#'
-#' @docType methods
-#'
 #' @export
 setMethod("predict", "pba",
-          function(object, newdata){
+          function(object, y){
 
-            balance.fromSBP(newdata, object@sbp)
+            balance.fromSBP(y, object@sbp)
+          }
+)
+
+#' @describeIn pba Method to plot \code{pba} model.
+#' @export
+setMethod("plot", signature(x = "pba", y = "missing"),
+          function(x, y, group, pb1 = 1, pb2 = 2, size.text = 18){
+
+            if(missing(group)) group <- "Unknown"
+            df <- cbind(x@pba[,c(pb1, pb2)], data.frame(group))
+            names <- colnames(df)
+            colnames(df) <- c("First", "Second", "group")
+            ggplot2::ggplot(df, ggplot2::aes_string(x = "First", y = "Second", col = "group")) +
+              ggplot2::geom_point() +
+              ggplot2::xlab(paste0(names[1], " [x var explained: ",
+                                   round(x@subvar[pb1]*100, 2), "%]")) +
+              ggplot2::ylab(paste0(names[2], " [x var explained: ",
+                                   round(x@subvar[pb2]*100, 2), "%]")) +
+              ggplot2::scale_color_brewer(palette = "Set2") +
+              ggplot2::labs(col = "Sample Group") +
+              ggplot2::theme_bw() +
+              ggplot2::theme(text = ggplot2::element_text(size = size.text)) +
+              ggplot2::theme(legend.position = "top")
+          }
+)
+
+#' @describeIn pba Method to plot \code{pba} model.
+#' @export
+setMethod("plot", signature(x = "pba", y = "matrix"),
+          function(x, y, group, pb1 = 1, pb2 = 2, size.text = 18){
+
+            if(missing(group)) group <- "Unknown"
+            df <- cbind(predict(x, y)[,c(pb1, pb2)], data.frame(group))
+            names <- colnames(df)
+            colnames(df) <- c("First", "Second", "group")
+            ggplot2::ggplot(df, ggplot2::aes_string(x = "First", y = "Second", col = "group")) +
+              ggplot2::geom_point() +
+              ggplot2::xlab(paste0(names[1], " [x var explained: ",
+                                   round(x@subvar[pb1]*100, 2), "%]")) +
+              ggplot2::ylab(paste0(names[2], " [x var explained: ",
+                                   round(x@subvar[pb2]*100, 2), "%]")) +
+              ggplot2::scale_color_brewer(palette = "Set2") +
+              ggplot2::labs(col = "Sample Group") +
+              ggplot2::theme_bw() +
+              ggplot2::theme(text = ggplot2::element_text(size = size.text)) +
+              ggplot2::theme(legend.position = "top")
+          }
+)
+
+#' @describeIn pba Method to plot \code{pba} model.
+#' @export
+setMethod("plot", signature(x = "pba", y = "data.frame"),
+          function(x, y, group, pb1 = 1, pb2 = 2, size.text = 18){
+
+            plot(x, as.matrix(y), group, pb1, pb2, size.text)
           }
 )
