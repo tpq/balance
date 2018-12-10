3e0c61a27af822199fc4c07a64134ee72ffb28f1
Thom Quinn <contacttomquinn@gmail.com>
2018-12-10 13:34:13 +1100

    Comment out philr test because of CRAN conflict

--- tests/testthat/test-sbp.R
+++ tests/testthat/test-sbp.R
@@ -1,20 +1,20 @@
-library(balance)
-
-h <- hclust(dist(cars[1:35,]))
-phylo <- ape::as.phylo(h)
-sbp1 <- philr::phylo2sbp(phylo)
-sbp2 <- sbp.fromHclust(h)
-colnames(sbp2) <- NULL
-
-test_that("sbp.fromHclust matches the ape and philr method", {
-
-  expect_equal(
-    sbp1[,1],
-    sbp2[,1]
-  )
-
-  expect_equal(
-    sbp.sort(sbp1),
-    sbp.sort(sbp2)
-  )
-})
+# library(balance)
+#
+# h <- hclust(dist(cars[1:35,]))
+# phylo <- ape::as.phylo(h)
+# sbp1 <- philr::phylo2sbp(phylo)
+# sbp2 <- sbp.fromHclust(h)
+# colnames(sbp2) <- NULL
+#
+# test_that("sbp.fromHclust matches the ape and philr method", {
+#
+#   expect_equal(
+#     sbp1[,1],
+#     sbp2[,1]
+#   )
+#
+#   expect_equal(
+#     sbp.sort(sbp1),
+#     sbp.sort(sbp2)
+#   )
+# })
